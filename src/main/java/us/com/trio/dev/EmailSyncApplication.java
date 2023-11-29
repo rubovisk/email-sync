4d4b5999fe8fc93726a23f083ef0f9c1244c006e
rubens daniel <daniel.bcmello@sp.senac.br>
2023-11-28 22:43:10 -0300

    initial version

---
+++ src/main/java/us/com/trio/dev/EmailSyncApplication.java
@@ -0,0 +1,12 @@
+package us.com.trio.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSyncApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EmailSyncApplication.class, args);
+	} 
+	
+}
